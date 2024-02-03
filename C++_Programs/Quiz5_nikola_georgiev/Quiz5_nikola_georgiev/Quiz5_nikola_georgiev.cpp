a9c9e5477d83dd95e89e5101afc956ab9b1a8f08
Nikola Georgiev <96839751+chavdibock@users.noreply.github.com>
2024-02-03 13:35:14 +0200

    Add files via upload

---
+++ C++_Programs/Quiz5_nikola_georgiev/Quiz5_nikola_georgiev/Quiz5_nikola_georgiev.cpp
@@ -0,0 +1,29 @@
+// Quiz5_nikola_georgiev.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Rectangle.h"
+using namespace std;
+
+int main()
+{
+
+    int a = 15, b = -1;
+
+
+    Rectangle first;
+    Rectangle second(a,b);
+    cout << first.getAera() << endl << second.getAera();
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
